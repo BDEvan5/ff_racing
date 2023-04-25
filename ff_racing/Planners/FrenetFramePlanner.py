084364c88d0762b473a4fe9929af81b835b22d23
Benjamin <benjaminevans316@gmail.com>
2023-04-25 17:01:48 +0200

    devel ff image processing

---
+++ ff_racing/Planners/FrenetFramePlanner.py
@@ -0,0 +1,122 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+from ff_racing.PlannerUtils.VehicleStateHistory import VehicleStateHistory
+from ff_racing.PlannerUtils.TrackLine import TrackLine
+
+import cv2 as cv
+from PIL import Image
+import os
+
+LOOKAHEAD_DISTANCE = 1
+WHEELBASE = 0.33
+MAX_STEER = 0.4
+MAX_SPEED = 6
+
+
+class FrenetFramePlanner:
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+        
+        if not os.path.exists(path): os.mkdir(path)
+        if not os.path.exists(path + "Scans/"): os.mkdir(path+ "Scans/")
+        # self.vehicle_state_history = VehicleStateHistory(name, "ff")
+        
+        fov2 = 4.7 / 2
+        self.angles = np.linspace(-fov2, fov2, 1080)
+        self.coses = np.cos(self.angles)
+        self.sines = np.sin(self.angles)
+        
+        self.counter = 0
+        
+    def plan(self, obs):
+        speed = obs['linear_vels_x'][0]
+        
+        center_line = self.run_centerline_extraction(obs['scans'][0])
+        
+        action = self.plan_frenet_action(center_line, speed)
+        
+        self.counter += 1
+        
+        return action
+        
+        
+    def run_centerline_extraction(self, scan):
+        save_path = self.path + "Scans/"
+        plt.figure()
+        plt.plot(scan*self.coses, scan*self.sines)
+        
+        plt.gca().set_aspect('equal', adjustable='box')
+        
+        plt.xticks([])
+        plt.yticks([])
+        ax = plt.gca()
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        
+        plt.tight_layout()
+        plt.savefig(save_path + f"{self.name}_{self.counter}.png", bounding_box_inches='tight', pad_inches=0, dpi=50)
+        
+        self.process_img()
+        
+        plt.show()
+    
+    def process_img(self):
+        img = cv.imread(self.path + "Scans/" + f"{self.name}_{self.counter}.png")
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        
+        edges = cv.Canny(img, 100, 250, apertureSize=5, L2gradient=True)
+
+        # Apply Hough Line Transform to detect lines
+        lines = cv.HoughLines(edges, 1, np.pi/180, 200)
+        
+        line_img = np.zeros_like(img)
+        if lines is not None:
+            for line in lines:
+                rho, theta = line[0]
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                x1 = int(x0 + 1000 * (-b))
+                y1 = int(y0 + 1000 * (a))
+                x2 = int(x0 - 1000 * (-b))
+                y2 = int(y0 - 1000 * (a))
+                cv.line(line_img, (x1, y1), (x2, y2), (255, 255, 255), 1)
+
+        # Display the image with the detected lines
+        cv.imshow('Lines', line_img)
+        cv.waitKey(0)
+        
+        # blur = cv.GaussianBlur(gray, (5, 5), cv.BORDER_DEFAULT)
+        # _, thresh = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+        # ret, thresh = cv.threshold(blur, 200, 255,
+                                # cv.THRESH_BINARY_INV)
+
+
+        # cv.imshow('ocv', gray)
+        # cv.waitKey(0)
+        
+
+        # c, _ = cv.findContours(gray,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+        # c, _ = cv.findContours(thresh,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE)
+        
+        cv.drawContours(img, c, -1, (0,255,0), 3)
+        cv.imshow('ocv', img)
+        cv.waitKey(10 * 1000)
+        
+        return img
+    
+        
+    def plan_frenet_action(self, center_line, speed):
+        
+        
+        return np.array([0, 5])
+        
+        
+    def done_callback(self, obs):
+        pass
+        
+        
---
+++ ff_racing/test_ff.py
@@ -0,0 +1,55 @@
+from ff_racing.f1tenth_gym.f110_env import F110Env
+
+from ff_racing.Planners.FrenetFramePlanner import FrenetFramePlanner
+
+import numpy as np
+
+RENDER_ENV = False
+# RENDER_ENV = True
+
+
+def run_simulation_loop_laps(env, planner, n_laps, n_sim_steps=10):
+    observation, reward, done, info = env.reset(poses=np.array([[0, 0, 0]]))
+    
+    for lap in range(n_laps):
+        while not done:
+            action = planner.plan(observation)
+            
+            mini_i = n_sim_steps
+            while mini_i > 0 and not done:
+                observation, reward, done, info = env.step(action[None, :])
+                mini_i -= 1
+
+            if RENDER_ENV: env.render('human')
+            
+        planner.done_callback(observation)
+        observation, reward, done, info = env.reset(poses=np.array([[0, 0, 0]]))   
+        
+
+def test_frenet_planner():
+    map_name = "aut" # "aut", "esp", "gbr", "mco"
+    agent_name = "devel_ff"
+    n_test_laps = 5
+    
+    env = F110Env(map=map_name, num_agents=1)
+    
+    agent_name = "MyFrenetPlanner"
+    planner = FrenetFramePlanner(agent_name, f"Data/{agent_name}/")
+    run_simulation_loop_laps(env, planner, n_test_laps, 10)
+  
+  
+  
+def test_endToEnd_agent_all_maps():
+    map_names = ["aut", "esp", "gbr", "mco"]
+    agent_name = "myFavouriteAgent_SAC"
+    n_test_laps = 2
+    
+    for map_name in map_names:
+        env = F110Env(map=map_name, num_agents=1)
+        TestAgent = TestSAC(agent_name, f"Data/{agent_name}/") # or DDPG, TD3
+        planner = EndToEndTest(TestAgent, map_name, agent_name)
+        run_simulation_loop_laps(env, planner, n_test_laps)
+  
+  
+if __name__ == "__main__":
+    test_frenet_planner()
