8901cc1947c5e4c37aae9ca9ab232e3fa93f006b
Benjamin <benjaminevans316@gmail.com>
2023-04-25 15:13:51 +0200

    Init commit

---
+++ ff_racing/f1tenth_gym/__init__.py
@@ -0,0 +1,5 @@
+from ff_racing.f1tenth_gym.f110_env import F110Env
+from ff_racing.f1tenth_gym.dynamic_models import *
+from ff_racing.f1tenth_gym.laser_models import *
+from ff_racing.f1tenth_gym.base_classes import *
+from ff_racing.f1tenth_gym.collision_models import *
---
+++ ff_racing/test_agents.py
@@ -0,0 +1,89 @@
+from ff_racing.f1tenth_gym.f110_env import F110Env
+from ff_racing.LearningAlgorithms.sac import TrainSAC, TestSAC
+from ff_racing.LearningAlgorithms.ddpg import TrainDDPG, TestDDPG
+from ff_racing.LearningAlgorithms.td3 import TrainTD3, TestTD3
+
+from ff_racing.Planners.TrainTrajectoryFollow import TrajectoryFollowTrain
+from ff_racing.Planners.TestTrajectoryFollow import TrajectoryFollowTest
+from ff_racing.Planners.TestEndToEnd import EndToEndTest
+from ff_racing.Planners.TrainEndToEnd import EndToEndTrain
+
+import numpy as np
+
+RENDER_ENV = False
+RENDER_ENV = True
+
+
+def run_simulation_loop_laps(env, planner, n_laps, n_sim_steps=10):
+    observation, reward, done, info = env.reset(poses=np.array([[0, 0, 0]]))
+    
+    for lap in range(n_laps):
+        while not done:
+            action = planner.plan(observation)
+            
+            mini_i = n_sim_steps
+            while mini_i > 0 and not done:
+                observation, reward, done, info = env.step(action[None, :])
+                mini_i -= 1
+
+            if RENDER_ENV: env.render('human')
+            
+        planner.done_callback(observation)
+        observation, reward, done, info = env.reset(poses=np.array([[0, 0, 0]]))   
+        
+
+def test_endToEnd_agent():
+    map_name = "aut" # "aut", "esp", "gbr", "mco"
+    agent_name = "myFavouriteAgent_SAC"
+    n_test_laps = 5
+    
+    env = F110Env(map=map_name, num_agents=1)
+    
+    TestAgent = TestSAC(agent_name, f"Data/{agent_name}/") # or DDPG, TD3
+    planner = EndToEndTest(TestAgent, map_name, agent_name)
+    run_simulation_loop_laps(env, planner, n_test_laps)
+  
+  
+def test_trajectoryFollowing_agent():
+    map_name = "aut" # "aut", "esp", "gbr", "mco"
+    agent_name = "myFavouriteAgentTF_SAC"
+    n_test_laps = 5
+    
+    env = F110Env(map=map_name, num_agents=1)
+
+    
+    test_agent = TestSAC(agent_name, f"Data/{agent_name}/") # or DDPG, TD3
+    planner = TrajectoryFollowTest(test_agent, map_name, agent_name)
+    run_simulation_loop_laps(env, planner, n_test_laps)
+  
+  
+def test_endToEnd_agent_all_maps():
+    map_names = ["aut", "esp", "gbr", "mco"]
+    agent_name = "myFavouriteAgent_SAC"
+    n_test_laps = 2
+    
+    for map_name in map_names:
+        env = F110Env(map=map_name, num_agents=1)
+        TestAgent = TestSAC(agent_name, f"Data/{agent_name}/") # or DDPG, TD3
+        planner = EndToEndTest(TestAgent, map_name, agent_name)
+        run_simulation_loop_laps(env, planner, n_test_laps)
+  
+  
+def test_trajectoryFollowing_agent_all_maps():
+    map_names = ["aut", "esp", "gbr", "mco"]
+    agent_name = "myFavouriteAgentTF_SAC"
+    n_test_laps = 2
+    
+    for map_name in map_names:
+        env = F110Env(map=map_name, num_agents=1)
+        test_agent = TestSAC(agent_name, f"Data/{agent_name}/") # or DDPG, TD3
+        planner = TrajectoryFollowTest(test_agent, map_name, agent_name)
+        run_simulation_loop_laps(env, planner, n_test_laps)
+  
+  
+if __name__ == "__main__":
+    # test_endToEnd_agent()
+    # test_trajectoryFollowing_agent()
+    test_endToEnd_agent_all_maps()
+    test_trajectoryFollowing_agent_all_maps()
+    
