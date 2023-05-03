9ec56857bf9136513ee9db5b65af3effa1f60fde
Benjamin <benjaminevans316@gmail.com>
2023-05-03 14:06:21 +0200

    Robust center line extraction

--- ff_racing/devel/ScanCenterline.py
+++ ff_racing/devel/ScanCenterline.py
@@ -53,5 +53,52 @@ def end_pt_line():
     plt.gca().set_aspect('equal', adjustable='box')
 
     plt.show()
+    
+def true_centerline():
+    lidar_data = np.load("Data/MyFrenetPlanner/ScanData/MyFrenetPlanner_0.npy")
 
-end_pt_line()
+    plt.figure()
+    fov2 = 4.7 / 2
+    angles = np.linspace(-fov2, fov2, 1080)
+    coses = np.cos(angles)
+    sines = np.sin(angles)
+
+    xs = coses * lidar_data
+    ys = sines * lidar_data
+
+    pts = np.hstack((xs[:, None], ys[:, None]))
+    pt_distances = np.linalg.norm(pts[1:] - pts[:-1], axis=1)
+    mid_idx = np.argmax(pt_distances)
+
+    length_1 = np.sum(pt_distances[:mid_idx])
+    length_2 = np.sum(pt_distances[mid_idx:])
+    
+    l1_ss = np.linspace(0, length_1, 50)
+    l1_ss = np.linspace(0, length_2, 50)
+    
+    l1_cs = np.cumsum(pt_distances[:mid_idx+1])
+    l2_cs = np.cumsum(pt_distances[mid_idx:])
+
+    l1_xs = np.interp(l1_ss, l1_cs, pts[:mid_idx+1, 0])
+    l1_ys = np.interp(l1_ss, l1_cs, pts[:mid_idx+1, 1])
+    l2_xs = np.interp(l1_ss, l2_cs, pts[mid_idx+1:, 0])
+    l2_ys = np.interp(l1_ss, l2_cs, pts[mid_idx+1:, 1])
+
+
+    c_xs = (l1_xs + l2_xs[::-1])/2
+    c_ys = (l1_ys + l2_ys[::-1])/2
+
+    plt.plot(xs, ys, 'x', label="Lidar")
+    
+    plt.plot(l1_xs, l1_ys, label="Center - 1")
+    plt.plot(l2_xs, l2_ys, label="Center - 2")
+    
+    plt.plot(c_xs, c_ys, '-', color='red', label="Center", linewidth=3)
+
+    plt.gca().set_aspect('equal', adjustable='box')
+
+    plt.legend()
+    plt.show()
+
+# end_pt_line()
+true_centerline()
